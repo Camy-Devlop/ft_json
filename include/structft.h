2e39f4bebd585d8b8c8aa7b3fd0379436af686ce
Camy-Devlop <adbaibi.ismail@me.com>
2025-06-27 15:01:46 +0200

    ajout de struct dansd le fichier

--- include/structft.h
+++ include/structft.h
@@ -1,12 +1,27 @@
 #ifndef STRUCTFT_H
 # define STRUCTFT_H
 
+typedef enum e_type
+{
+	NULLF;
+	OBJECT,
+	TABLEAU,
+	INT,
+	FLOAT,
+	DOUBLE,
+	STRING
+}	t_type;
+
+typedef struct s_obejct
+{
+	void	*name;
+	void	*value;
+}	t_object;
+
 typedef struct s_element
 {
-	bloc
-	element 
-	entiter
-	
+	t_type		type;
+	t_object	*objet;
 }t_element
 
 
